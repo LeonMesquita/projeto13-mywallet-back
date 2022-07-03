dfcc7e193c2a9cf2f980eb635b22627ad8a8fedc
LeonMesquita <leonmesquita16@gmail.com>
2022-07-03 15:35:45 -0300

    .

--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ const app = express();
 
 
 
-
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    app.use(cors());
     next();
 });
 
