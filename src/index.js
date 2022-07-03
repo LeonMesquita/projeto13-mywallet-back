0a07fc6cba4a208dff8287f88f831a2e10429098
LeonMesquita <leonmesquita16@gmail.com>
2022-07-03 15:15:20 -0300

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-/*
+
 
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
@@ -16,7 +16,7 @@ app.use(function(req, res, next){
     next();
 });
 
-*/
+
 
 
 
