848937901ac1c2c5f7ce92706e1ccf3d4000f76f
LeonMesquita <leonmesquita16@gmail.com>
2022-07-04 13:00:44 -0300

    .

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -6,8 +6,14 @@ import dayjs from 'dayjs';
 dayjs().format()
 
 export async function addRegister(req, res){
-    const session = res.locals.session;
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
     const registerBody = req.body;
+
+    const session = await db.collection('sessions').findOne({token});
+    if(!session) return res.sendStatus(401);
+    
+
     await db.collection("registers").insertOne({
         ...registerBody,
         userId: session.userId,
@@ -19,22 +25,46 @@ export async function addRegister(req, res){
 
 
 export async function getRegisters(req, res){
-    const session = res.locals.session;
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    const session = await db.collection('sessions').findOne({token});
+    if(!session) return res.sendStatus(401);
+
     const registers = await db.collection('registers').find({
         userId: new objectId(session.userId)
     }).toArray();
+
     const user = await db.collection('users').findOne({_id: new ObjectId(session.userId)});
+   // console.log(registers);
+    console.log(user);
+
+
+
     return res.send({registers: registers, userName: user.name});
 }
 
 export async function deleteRegister(req, res){
+    console.log('entrou')
+    const { authorization } = req.headers;
+    console.log(authorization);
+    const token = authorization?.replace("Bearer ", "");
+
+    const session = await db.collection('sessions').findOne({token});
+    if(!session) return res.sendStatus(401);
+
     const { registerId } = req.body;
+    console.log(registerId);
+
     try{
         await db.collection('registers').deleteOne({_id: new objectId(registerId)});
         return res.sendStatus(200);
+
     }catch(error){
+        console.log(error);
         return res.sendStatus(400);
 
     }
     
+
 }
